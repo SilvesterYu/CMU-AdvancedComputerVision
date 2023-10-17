53f67430d1f98c35a0cc12e4872997d095dfc7bf
Lifan Yu (Silvey) <ly1164@nyu.edu>
2023-10-17 16:52:47 -0400

    debug

--- lifany_hw3/q2_1_eightpoint.py
+++ lifany_hw3/q2_1_eightpoint.py
@@ -28,14 +28,18 @@ def eightpoint(pts1, pts2, M):
     # YOUR CODE HERE
 
     # (1) Normalize the input pts1 and pts2 using the matrix T.
-    npts1, npts2 = pts1/M, pts2/M
+    N = pts1.shape[0]
+    pts11 = np.append(pts1, np.ones((N, 1)), axis=1)
+    pts21 = np.append(pts2, np.ones((N, 1)), axis=1)
+    T = np.array([[1/M, 0, 0],
+                  [0, 1/M, 0],
+                  [0, 0, 1]])
+    npts1, npts2 = np.matmul(pts11, T), np.matmul(pts21, T)
 
     # (2) Setup the eight point algorithm's equation, which is AF(:) = 0
-    N = pts1.shape[0]
     A = np.zeros(shape=(N, 9))
-    npts11, npts21 = np.append(npts1, np.ones((N, 1)), axis=1), np.append(npts2, np.ones((N, 1)), axis=1)
     for i in range(N):
-      row = np.matmul(npts11[i].reshape((3, 1)), npts21[i].reshape((1, 3))).reshape((9, ))
+      row = np.matmul(npts2[i].reshape((3, 1)), npts1[i].reshape((1, 3))).reshape((9, ))
       A[i] = row
 
     # (3) Solve for the least square solution using SVD. 
@@ -43,20 +47,19 @@ def eightpoint(pts1, pts2, M):
     # Take last row of VT in special case of SVD when Ax = 0
     # Math reference: www.cse.unr.edu/~bebis/CS791E/Notes/SVD.pdf
     F = VT[-1, :].reshape((3, 3))
+    
 
     # (4) Use the function `_singularize` (provided) to enforce the singularity condition. 
     F = _singularize(F)
+    print("singularize F", F)
 
     # (5) Use the function `refineF` (provided) to refine the computed fundamental matrix. 
-    F = refineF(F, npts1, npts2)
-    t = np.diag([1/M, 1/M, 1])
-    f2 = np.matmul(t.T, (F @ t))
-    f2 = f2 / f2[2, 2]
-
+    F = refineF(F, npts1[:, :-1], npts2[:, :-1])
+    
     # (6) Unscale the fundamental matrix
-
-    print(F)
-    print(f2)
+    f2 = np.matmul(T, np.matmul(F, T))
+    f2 = f2 / f2[2, 2]
+    print("F", F)
 
     return f2
 
