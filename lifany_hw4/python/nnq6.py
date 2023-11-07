33e658e33ffc18424b1ffe22688f3be8d146b896
Lifan Yu (Silvey) <ly1164@nyu.edu>
2023-11-06 22:54:45 -0500

    +

--- lifany_hw4/python/nnq6.py
+++ lifany_hw4/python/nnq6.py
@@ -4,77 +4,13 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+import matplotlib.pyplot as plt
 
-# convert numpy array to dataloader
-def np2loader(X, y, batchsize=128, shuffling=True):
-    y = y.argmax(axis=1)
-    X = torch.from_numpy(np.float32(X))
-    y = torch.from_numpy(y)
-    data = torch.utils.data.TensorDataset(X, y)
-    if batchsize != None:
-        loader = torch.utils.data.DataLoader(data, batch_size=batchsize, shuffle=shuffling)
-    else:
-        loader = torch.utils.data.DataLoader(data, shuffle=shuffling)
-    return loader
+####################################################################################################
+######################################## NEURAL NETWORKS ###########################################
+####################################################################################################
 
-def training_loop(myNet, trainLoader, device, max_iters, learning_rate, lossf, optimizer, fname):
-    # Training loop
-    # Initialize the network
-    myNet = myNet.to(device)
-    print(myNet)
-    for itr in range(max_iters):
-        total_loss = 0.0
-        total_correct = 0.0
-        total_instances = 0
-
-        for times, data in enumerate(trainLoader):
-            # print(data)
-            inputs, labels = data[0].to(device), data[1].to(device)
-            inputs = inputs.view(inputs.shape[0], -1)
-
-            # Zero the parameter gradients
-            optimizer.zero_grad()
-
-            # Foward, backward, optimize
-            outputs = myNet(inputs)
-            loss = lossf(outputs, labels)
-            loss.backward()
-            optimizer.step()
-
-            # Total loss
-            total_loss += loss.item()
-
-            with torch.no_grad():
-                # average accuracy
-                classifications = torch.argmax(myNet(inputs), dim=1)
-                # labels = torch.argmax(labels, dim=1)
-                correct_predictions = sum(classifications==labels).item()
-                total_correct+=correct_predictions
-                total_instances+=len(inputs)
-            
-        accuracy = round(total_correct/total_instances, 4)
-
-        #     # since we're not training, we don't need to calculate the gradients for our outputs
-        #     with torch.no_grad():
-        #         outputs = myNet(inputs)
-        #         # the class with the highest energy is what we choose as prediction
-        #         _, predicted = torch.max(outputs.data, 1)
-        #         total_instances += labels.size(0)
-        #         total_correct += (predicted == labels).sum().item()
-        # accuracy = total_correct / total_instances
-        
-        if itr % 10 == 0:
-            
-            print(
-                "itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(
-                    itr, total_loss, accuracy
-                )
-            )
-
-    # save the weights
-    torch.save(myNet.state_dict(), fname + '.pth')
-
-# tutorial: https://clay-atlas.com/us/blog/2021/04/22/pytorch-en-tutorial-4-train-a-model-to-classify-mnist/
+# TUTORIAL: https://clay-atlas.com/us/blog/2021/04/22/pytorch-en-tutorial-4-train-a-model-to-classify-mnist/
 
 # for Q6.1.1
 class Net(nn.Module):
@@ -114,3 +50,117 @@ class CNN(nn.Module):
         self.fc1 = nn.Linear(9216, 128)
         # Second fully connected layer that outputs our 10 labels
         self.fc2 = nn.Linear(128, 10)
+
+####################################################################################################
+######################################## HELPER FUNCTIONS ##########################################
+####################################################################################################
+
+# convert numpy array to dataloader
+def np2loader(X, y, batchsize=128, shuffling=True):
+    y = y.argmax(axis=1)
+    X = torch.from_numpy(np.float32(X))
+    y = torch.from_numpy(y)
+    data = torch.utils.data.TensorDataset(X, y)
+    if batchsize != None:
+        loader = torch.utils.data.DataLoader(data, batch_size=batchsize, shuffle=shuffling)
+    else:
+        loader = torch.utils.data.DataLoader(data, shuffle=shuffling)
+    return loader
+
+def training_loop(myNet, trainLoader, validLoader, device, max_iters, learning_rate, lossf, optimizer, fname):
+    # Training loop
+    # Initialize the network
+    myNet = myNet.to(device)
+    print(myNet)
+    train_loss_list, train_acc_list, val_loss_list, val_acc_list = [], [], [], []
+    for itr in range(max_iters):
+        accuracy, train_loss = evaluate_model(myNet, trainLoader, lossf, device)
+        train_acc_list.append(accuracy)
+        train_loss_list.append(train_loss)
+
+        val_accuracy, val_loss = evaluate_model(myNet, validLoader, lossf, device)
+        val_acc_list.append(val_accuracy)
+        val_loss_list.append(val_loss)
+
+        myNet.train()
+
+        total_loss = 0.0
+        total_correct = 0.0
+        total_instances = 0
+
+        for times, data in enumerate(trainLoader):
+            inputs, labels = data[0].to(device), data[1].to(device)
+            inputs = inputs.view(inputs.shape[0], -1)
+
+            # Zero the parameter gradients
+            optimizer.zero_grad()
+
+            # Foward, backward, optimize
+            outputs = myNet(inputs)
+            loss = lossf(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # Total loss
+            total_loss += loss.item()
+
+            with torch.no_grad():
+                # average accuracy
+                classifications = torch.argmax(myNet(inputs), dim=1)
+                # labels = torch.argmax(labels, dim=1)
+                correct_predictions = sum(classifications==labels).item()
+                total_correct+=correct_predictions
+                total_instances+=len(inputs)
+            
+        ave_accuracy = round(total_correct/total_instances, 4)
+        
+        if itr % 10 == 0:
+            
+            print(
+                "itr: {:02d} \t loss: {:.2f} \t acc : {:.2f} \t eval_acc : {:.2f}".format(
+                    itr, total_loss, ave_accuracy,val_accuracy
+                )
+            )
+
+    # save the weights
+    torch.save(myNet.state_dict(), fname)
+
+    # visualize
+    plot_train_valid(train_acc_list, val_acc_list, "accuracy")
+    plot_train_valid(train_loss_list, val_loss_list, "average loss")
+
+def evaluate_model(myNet, validLoader, lossf, device):
+    myNet.eval()
+    total_loss = 0.0
+    total_correct = 0.0
+    total_instances = 0
+    for times, data in enumerate(validLoader):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        inputs = inputs.view(inputs.shape[0], -1)
+        outputs = myNet(inputs)
+        loss = lossf(outputs, labels)
+        # Total loss
+        total_loss += loss.item()
+        with torch.no_grad():
+            # average accuracy
+            classifications = torch.argmax(myNet(inputs), dim=1)
+            # labels = torch.argmax(labels, dim=1)
+            correct_predictions = sum(classifications==labels).item()
+            total_correct+=correct_predictions
+            total_instances+=len(inputs)
+    accuracy = round(total_correct/total_instances, 4)
+    return accuracy, total_loss/total_instances
+
+# Plot train and valid loss / accuracies
+def plot_train_valid(train_data, valid_data, datatype):
+    plt.plot(range(len(train_data)), train_data, label="training")
+    plt.plot(range(len(valid_data)), valid_data, label="validation")
+    plt.xlabel("epoch")
+    plt.ylabel(datatype)
+    plt.xlim(0, len(train_data) - 1)
+    plt.ylim(0, None)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
--- lifany_hw4/python/run_q6_fully_connected.py
+++ lifany_hw4/python/run_q6_fully_connected.py
@@ -15,18 +15,6 @@ from nnq6 import *
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 print('GPU State:', device)
 
-# Transform to Tensor type and normalize
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5,), (0.5,)),]
-)
-
-# # Dataloaders
-# trainSet = datasets.MNIST(root='MNIST', download=True, train=True, transform=transform)
-# testSet = datasets.MNIST(root='MNIST', download=True, train=False, transform=transform)
-# trainLoader = dset.DataLoader(trainSet, batch_size=64, shuffle=True)
-# testLoader = dset.DataLoader(testSet, batch_size=64, shuffle=False)
-
 # Load original data
 np.random.seed(42)
 
@@ -47,85 +35,15 @@ myNet = Net()
 
 # Parameters
 max_iters = 200
-learning_rate = 1e-1
+learning_rate = 5e-2
 lossf = nn.CrossEntropyLoss()
 optimizer = optim.SGD(myNet.parameters(), lr=learning_rate)
-fname = 'q6_fully_connected'
+fname = 'q6_fully_connected.pth'
 
 # Training loop
-training_loop(myNet, trainLoader, device, max_iters, learning_rate, lossf, optimizer, fname)
+training_loop(myNet, trainLoader, validLoader, device, max_iters, learning_rate, lossf, optimizer, fname)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# for itr in range(max_iters):
-#     total_loss = 0.0
-#     total_correct = 0.0
-#     total_instances = 0
-
-#     for times, data in enumerate(trainLoader):
-#         # print(data)
-#         inputs, labels = data[0].to(device), data[1].to(device)
-#         inputs = inputs.view(inputs.shape[0], -1)
-
-#         # Zero the parameter gradients
-#         optimizer.zero_grad()
-
-#         # Foward, backward, optimize
-#         outputs = myNet(inputs)
-#         loss = lossf(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-
-#         # Total loss
-#         total_loss += loss.item()
-
-#         with torch.no_grad():
-#             # average accuracy
-#             classifications = torch.argmax(myNet(inputs), dim=1)
-#             # labels = torch.argmax(labels, dim=1)
-#             correct_predictions = sum(classifications==labels).item()
-#             total_correct+=correct_predictions
-#             total_instances+=len(inputs)
-        
-#     accuracy = round(total_correct/total_instances, 4)
-
-#     #     # since we're not training, we don't need to calculate the gradients for our outputs
-#     #     with torch.no_grad():
-#     #         outputs = myNet(inputs)
-#     #         # the class with the highest energy is what we choose as prediction
-#     #         _, predicted = torch.max(outputs.data, 1)
-#     #         total_instances += labels.size(0)
-#     #         total_correct += (predicted == labels).sum().item()
-#     # accuracy = total_correct / total_instances
-    
-#     if itr % 10 == 0:
-        
-#         print(
-#             "itr: {:02d} \t loss: {:.2f} \t acc : {:.2f}".format(
-#                 itr, total_loss, accuracy
-#             )
-#         )
-
-# # save the weights
-# torch.save(myNet.state_dict(), 'q6_fully_connected.pth')
-
-
-
+# Test
+myNet.load_state_dict(torch.load(fname))
+test_acc, _ = evaluate_model(myNet, testLoader, lossf, device)
+print("Test accuracy: ", test_acc)
