86ae4d50a3a9ffa05efa356b4c42504d1bd20a68
SilvesterYu <ly1164@nyu.edu>
2023-11-18 14:57:44 -0500

    initial

---
+++ lifany_hw5/README.md
@@ -0,0 +1,23 @@
+## HOMEWORK 5: PHOTOMETRIC STEREO
+
+#### Instructions
+
+Follow `16_820_Advanced_Computer_Vision_Photometric_Stereo_HW5.pdf`.
+
+Make sure to check the sections on `FAQ` and `Helpful Concepts` in the pdf.
+
+To install the required packages for the homework run:
+
+```
+pip install -r requirements.txt
+```
+
+The packages have been tested with python `3.10.12`
+
+#### HW3 - Submission checklist
+
+Submit your write-up and code to Gradescope.
+
+* **Writeup:** The write-up should be a pdf file named \<**AndrewId**\>_hw3.pdf
+* **Code:** The code should be submitted as a zip named \<**AndrewId**\>.zip 
+
---
+++ lifany_hw5/src/q2.py
@@ -0,0 +1,87 @@
+# ##################################################################### #
+# 16820: Computer Vision Homework 5
+# Carnegie Mellon University
+# 
+# Nov, 2023
+# ##################################################################### #
+
+import numpy as np
+import matplotlib.pyplot as plt
+from q1 import (
+    loadData,
+    estimateAlbedosNormals,
+    displayAlbedosNormals,
+    estimateShape,
+)
+from q1 import estimateShape
+from utils import enforceIntegrability, plotSurface
+
+def estimatePseudonormalsUncalibrated(I):
+    """
+    Question 2 (b)
+
+    Estimate pseudonormals without the help of light source directions.
+
+    Parameters
+    ----------
+    I : numpy.ndarray
+        The 7 x P matrix of loaded images
+
+    Returns
+    -------
+    B : numpy.ndarray
+        The 3 x P matrix of pseudonormals
+
+    L : numpy.ndarray
+        The 3 x 7 array of lighting directions
+
+    """
+
+    B = None
+    L = None
+    # Your code here
+    return B, L
+
+
+def plotBasRelief(B, mu, nu, lam):
+    """
+    Question 2 (f)
+
+    Make a 3D plot of of a bas-relief transformation with the given parameters.
+
+    Parameters
+    ----------
+    B : numpy.ndarray
+        The 3 x P matrix of pseudonormals
+
+    mu : float
+        bas-relief parameter
+
+    nu : float
+        bas-relief parameter
+
+    lambda : float
+        bas-relief parameter
+
+    Returns
+    -------
+        None
+
+    """
+
+    # Your code here
+    pass
+
+    if __name__ == "__main__":
+        pass
+        # Part 2 (b)
+        # Your code here
+
+        # Part 2 (d)
+        # Your code here
+
+        # Part 2 (e)
+        # Your code here
+
+        # Part 2 (f)
+        # Your code here
